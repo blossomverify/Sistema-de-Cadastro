167be178384353c17e7ecc31a50eb9308edde1ba
Murilo <15041329630@ulife.com.br>
2026-04-07 18:59:10 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    String nome;
+    String email;
+    String telefone;
+    public Usuario(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
